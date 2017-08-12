d6b0074c9a4d60d6187c8619118d904001dee34b
Tobias Delp <tobias.delp@web.de>
2017-08-12 12:50:41 +0900

    solved problem 4

--- src/problem_4/ts/problem_4.ts
+++ src/problem_4/ts/problem_4.ts
@@ -1,18 +1,40 @@
 /**
  * Created by tobias on 11.08.17.
  *
- * get the largest palindrome product from the product of two 3-digit numbers
+ * get the largest palindrome from the product of two 3-digit numbers
  *
  */
 
-var isPalindrome = function (str: number|string):boolean {
-    var input:string = str.toString();
-    var rev :string = input.split('').reverse().join('');
+var solveProblem = function (): string {
+    var arrA: number[] = Array.apply(null, Array(1000)).map((_, i) => i).reverse();
+    var arrB: number[] = Array.apply(null, Array(1000)).map((_, i) => i).reverse();
 
-    document.write("Last Factor: " + (rev === input) + "<br />");
+    var result: number = 0;
 
-    return rev === input;
+    arrA.forEach(nA => {
+        arrB.forEach(nB => {
+            let cache = nA*nB;
+            //only consider results which are bigger than an already found result
+            if(cache > result) {
+                if(isPalindrome(cache)){
+                    console.log(result);
+                    result = cache;
+                    return;
+                }
+            } else {
+                return;
+            }
+        });
+    });
 
+    return result.toString();
 };
 
-isPalindrome(7999);
+var isPalindrome = function (str: number|string): boolean {
+    var input: string = str.toString();
+    var rev: string = input.split('').reverse().join('');
+
+    return rev === input;
+};
+
+document.write("Largest Palindrome: " + solveProblem() + "<br />");
